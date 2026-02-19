c2eebe9375e548b14410df39d4dc9823e54381e2
ismdaniil <ism-daniil@mail.ru>
2026-02-19 17:23:43 +0700

    Коммит №5: (РЕФАКТОРИНГ) IsCityExist сделан константным, цикл заменен на std::find

--- CitiesGame/CitiesGame/Game.h
+++ CitiesGame/CitiesGame/Game.h
@@ -1,16 +1,15 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <algorithm> // для std::find
 
 class Game {
 private:
-    std::vector<std::string> citiesDatabase = { "Москва" }; // Минимальная база для одного теста
+    std::vector<std::string> citiesDatabase = { "Москва" };
 
 public:
-    bool IsCityExist(const std::string& city) {
-        for (const auto& c : citiesDatabase) {
-            if (c == city) return true;
-        }
-        return false;
+    bool IsCityExist(const std::string& city) const {
+        // Используем алгоритм find вместо ручного цикла
+        return std::find(citiesDatabase.begin(), citiesDatabase.end(), city) != citiesDatabase.end();
     }
 };
